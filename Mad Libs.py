6a044a6f3133ba489770a41aa514e1d522acb377
David Evans <evansdav.a@gmail.com>
2019-08-29 15:52:30 -0700

    Added tag for plural version of NOUN_2

--- Mad Libs.py
+++ Mad Libs.py
@@ -18,19 +18,40 @@ def get_words(dict):
     keys = list(dict.keys())
     random.shuffle(keys)
 
+    # Loop inspired by Austin Marshall on Stack Overflow 
+    # https://stackoverflow.com/questions/7785672/how-to-iterate-through-dict-in-random-order-in-python
     for key in keys:
-       while word_dict[key] == "":
-         word_dict[key] = input("Please input a " + get_type(key) + ": ")
-         if word_dict[key] == "":
-            print("I didn't get that. Please try again")
+       while dict[key] == "" and key != "NOUN_2P":
+            if key.startswith("a"):
+                dict[key] = input("Please input an " + get_type(key) + ": ")
+            else:
+                dict[key] = input("Please input a " + get_type(key) + ": ")
 
+            if dict[key] == "":
+                print("I didn't get that. Please try again")
+
+    dict["NOUN_2P"] = p.plural(dict["NOUN_2"])
+
+
+# Replace all words in the story
+def replace_words(story, dict):
+    new_story = story
+    for key in dict:
+        if key == "NOUN_2P":
+            new_story = new_story.replace("["+key+"]", p.plural(dict[key]))
+        else:
+            new_story = new_story.replace("["+key+"]", dict[key])
+
+    return new_story
 
 # List of keys that will be used for replacement
 word_dict = {
     "NAME": "",
     "NOUN_1": "",
     "NOUN_2": "",
+    "NOUN_2P": "",
     "NOUN_3": "",
+    "NOUN_4": "",
     "PLURAL NOUN_1": "",
     "PLURAL NOUN_2": "",
     "VERB_1": "",
@@ -41,7 +62,7 @@ word_dict = {
     "ADVERB_1": "",
     }
 
-# Template for story
+# Original version of story
 og_story = """
     Isaac Asimov's "Three Laws of Robotics"
 
@@ -58,17 +79,17 @@ og_story = """
     fall victim to accidents on a horrific scale.
     """
 
-# Story ready to be libbed
-story = """
+# Story template ready to be libbed
+template = """
     [NAME]'s "Three Laws of [FIELD OF STUDY]"
 
     1. A [NOUN_1] may not [VERB_1] a [NOUN_2] or, through inaction, allow a
        [NOUN_2] to [VERB_1].
 
-    2. A [NOUN_1] must obey [PLURAL NOUN_1] given it by [NOUN_2] except where
+    2. A [NOUN_1] must obey [PLURAL NOUN_1] given it by [NOUN_2P] except where
        such [PLURAL NOUN_1] would conflict with the First Law.
 
-    3. A [NOUN_1] must [VERB_2] its own [NOUN] as long as such [VERB_2] does
+    3. A [NOUN_1] must [VERB_2] its own [NOUN_4] as long as such [VERB_2] does
        not conflict with the First or Second Law.
 
     These laws ensure that a [NOUN_3] which relies [ADVERB_1] on [NOUN_1] does
@@ -76,6 +97,4 @@ story = """
     """
 
 get_words(word_dict)
-print(word_dict)
-
-print(p.plural(input()))
+print(replace_words(template, word_dict))
