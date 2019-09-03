bf4c10cfa424829219caf2e0a6c6042004ee0ad2
David Evans <evansdav.a@gmail.com>
2019-09-03 09:26:47 -0700

    Fixed grammatical error with some options

--- Mad Libs.py
+++ Mad Libs.py
@@ -26,6 +26,7 @@ def get_words(dict):
             if dict[key] == "":
                 print("I didn't get that. Please try again")
 
+    # Special modifications of other words
     dict["NOUN_2P"] = pluralize(dict["NOUN_2"])
     dict["VERB_1P"] = conjugate(dict["VERB_1"], "p")
 
@@ -78,18 +79,19 @@ og_story = """
 template = """
     [NAME]'s "Three Laws of [FIELD OF STUDY]"
 
-    1. A [NOUN_1] may not [VERB_1] a [NOUN_2] or, through inaction, allow a
+    1. A(n) [NOUN_1] may not [VERB_1] a [NOUN_2] or, through inaction, allow a
        [NOUN_2] to be [VERB_1P].
 
-    2. A [NOUN_1] must obey [PLURAL NOUN_1] given it by [NOUN_2P] except where
-       such [PLURAL NOUN_1] would conflict with the First Law.
+    2. A(n) [NOUN_1] must obey [PLURAL NOUN_1] given it by [NOUN_2P] except
+       where such [PLURAL NOUN_1] would conflict with the First Law.
 
-    3. A [NOUN_1] must [VERB_2] its own [NOUN_4] as long as such [VERB_2] does
-       not conflict with the First or Second Law.
+    3. A(n) [NOUN_1] must [VERB_2] its own [NOUN_4] as long as such [VERB_2]
+       does not conflict with the First or Second Law.
 
     These laws ensure that a [NOUN_3] which relies [ADVERB_1] on [NOUN_1] does
     not [VERB_3] to [PLURAL NOUN_2] on a [ADJECTIVE_1] scale.
     """
 
+# Run program
 get_words(word_dict)
 print(replace_words(template, word_dict))
